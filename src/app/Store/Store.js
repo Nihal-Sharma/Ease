e50b894d8fd6cc12d962631b39fdb9e3fce97bc0
Nihal-Sharma <72800662+Nihal-Sharma@users.noreply.github.com>
2024-09-26 12:43:10 +0530

    Signup API

--- src/app/Store/Store.js
+++ src/app/Store/Store.js
@@ -2,7 +2,7 @@ import {create} from 'zustand'
 const local_api = "http://localhost:3000";
 const server_api = "https://ai-ease.vercel.app"
 export const useStore = create((set) => ({
-    url : local_api,
+    url : server_api,
     
     count: 1,
     inc: () => set((state) => ({ count: state.count + 1 })),

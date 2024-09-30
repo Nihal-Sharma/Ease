f0ba0237ea290e5feea42e661fb899460ff416fb
Nihal-Sharma <72800662+Nihal-Sharma@users.noreply.github.com>
2024-09-30 23:41:18 +0530

    bug test

--- src/app/webscreens/account/page.jsx
+++ src/app/webscreens/account/page.jsx
@@ -3,23 +3,23 @@ import { useStore } from '@/app/Store/Store'
 import React from 'react'
 
 const Page = () => {
-  const dataAvailable = useStore((state)=>(state.dataAvailable))
-  const details = useStore((state)=> state.details)
-  const token = localStorage.getItem("token")
+  // const dataAvailable = useStore((state)=>(state.dataAvailable))
+  // const details = useStore((state)=> state.details)
+  // const token = localStorage.getItem("token")
 
-  useEffect(()=>{
-    if (dataAvailable ===true){
-      console.log("Data Already Exists")
-    }
-    else{
-      if(token){
-        console.log("request sent for details via token")
-      }
-    }
-  },[])
+  // useEffect(()=>{
+  //   if (dataAvailable ===true){
+  //     console.log("Data Already Exists")
+  //   }
+  //   else{
+  //     if(token){
+  //       console.log("request sent for details via token")
+  //     }
+  //   }
+  // },[])
    
   return (
-    <div>welcome {details.username}</div>
+    <div>welcome nihal</div>
   )
 }
 
